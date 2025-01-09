807d39b3773dee4f50fccb8ed96a47c012cf828d
vyominiiiiiiii <102034432+vyominiiiiiiii@users.noreply.github.com>
2025-01-09 22:55:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+var test1="Life is good";
+var test2='Life is good';
+var test3=`Life is good`;
+console.log(test1);
+console.log(test2);
+console.log(test3);
+
+var str1='They are called the "Avengers"';
+console.log(str1);
+console.log(str1.length);
+
+console.log(str1[5]);
+
+console.log("If you won't have fruits\\fruit juice and vegetables\\green leafy vegetables,then you won't be healthy");
